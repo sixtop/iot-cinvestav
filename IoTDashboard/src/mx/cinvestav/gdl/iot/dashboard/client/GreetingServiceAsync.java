3acae7051ba5e17aa5f5c0b30ff91eb97dd12a7f
Lea Vega <Lea Vega@LVR>
2014-11-21 13:04:37 -0600

    Web page

---
+++ IoTDashboard/src/mx/cinvestav/gdl/iot/dashboard/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package mx.cinvestav.gdl.iot.dashboard.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
