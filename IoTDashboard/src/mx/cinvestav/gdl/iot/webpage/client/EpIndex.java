9c4cc5bfefcf81608977dd845c74082e4c04f456
Lea Vega <Lea Vega@LVR>
2014-12-16 17:39:54 -0600

    Maps

--- IoTDashboard/src/mx/cinvestav/gdl/iot/webpage/client/EpIndex.java
+++ IoTDashboard/src/mx/cinvestav/gdl/iot/webpage/client/EpIndex.java
@@ -1,7 +1,5 @@
 package mx.cinvestav.gdl.iot.webpage.client;
 
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -9,31 +7,37 @@ import com.googlecode.gwt.charts.client.ChartLoader;
 import com.googlecode.gwt.charts.client.ChartPackage;
 import com.googlecode.gwt.charts.client.ColumnType;
 import com.googlecode.gwt.charts.client.DataTable;
+import com.googlecode.gwt.charts.client.corechart.LineChart;
+import com.googlecode.gwt.charts.client.corechart.LineChartOptions;
 import com.googlecode.gwt.charts.client.geochart.GeoChart;
 import com.googlecode.gwt.charts.client.geochart.GeoChartColorAxis;
 import com.googlecode.gwt.charts.client.geochart.GeoChartOptions;
+import com.googlecode.gwt.charts.client.options.HAxis;
+import com.googlecode.gwt.charts.client.options.VAxis;
+
 
 public class EpIndex extends IoTEntryPoint
 {
 	private GeoChart geoChart;
-
+	private LineChart chart;
+	
+	
 	public void continueModuleLoad()
 	{
-		ChartLoader chartLoader = new ChartLoader(ChartPackage.GEOCHART);
-		chartLoader.loadApi(new Runnable()
-		{
+		ChartLoader chartLoader = new ChartLoader(ChartPackage.CORECHART);
+		chartLoader.loadApi(new Runnable() {
 
 			@Override
-			public void run()
-			{
+			public void run() {
 				DecoratorPanel chartPanel = new DecoratorPanel();
 				chartPanel.add(getGeoChart());
-
+	
 				RootPanel.get("chart").add(chartPanel);
-
 				draw();
 			}
 		});
+		
+	
 	}
 
 	private Widget getGeoChart()
@@ -45,35 +49,40 @@ public class EpIndex extends IoTEntryPoint
 		return geoChart;
 	}
 
-	private void draw()
-	{
-		// Prepare the data
-				DataTable dataTable = DataTable.create();
-				dataTable.addColumn(ColumnType.STRING, "Country");
-				dataTable.addColumn(ColumnType.NUMBER, "Popularity");
-				dataTable.addRows(6);
-				dataTable.setValue(0, 0, "Germany");
-				dataTable.setValue(0, 1, 200);
-				dataTable.setValue(1, 0, "United States");
-				dataTable.setValue(1, 1, 300);
-				dataTable.setValue(2, 0, "Brazil");
-				dataTable.setValue(2, 1, 400);
-				dataTable.setValue(3, 0, "Canada");
-				dataTable.setValue(3, 1, 500);
-				dataTable.setValue(4, 0, "France");
-				dataTable.setValue(4, 1, 600);
-				dataTable.setValue(5, 0, "RU");
-				dataTable.setValue(5, 1, 700);
+	
+	private void draw(){
+		DataTable dataTable = DataTable.create();
+		dataTable.addColumn(ColumnType.STRING, "Country");
+		dataTable.addColumn(ColumnType.STRING, "CityFarm");
+		
+		dataTable.addRows(6);
+		dataTable.setValue(0, 0, "Germany");
+		dataTable.setValue(0, 1, "200");
+		dataTable.setValue(1, 0, "United States");
+		dataTable.setValue(1, 1, "MIT");
+		dataTable.setValue(2, 0, "Brazil");
+		dataTable.setValue(2, 1, "400");
+		dataTable.setValue(3, 0, "Canada");
+		dataTable.setValue(3, 1, "500");
+		dataTable.setValue(4, 0, "France");
+		dataTable.setValue(4, 1, "600");
+		dataTable.setValue(5, 0, "RU");
+		dataTable.setValue(5, 1, "700");
 
-				// Set options
-				GeoChartOptions options = GeoChartOptions.create();
-				GeoChartColorAxis geoChartColorAxis = GeoChartColorAxis.create();
-			//	geoChartColorAxis.setColors("green", "yellow", "red");
-				options.setColorAxis(geoChartColorAxis);
-				options.setDatalessRegionColor("gray");
+		// Set options
+		GeoChartOptions options = GeoChartOptions.create();
+		options.setHeight(500);
+		options.setWidth(900);
+		
+		
+		GeoChartColorAxis geoChartColorAxis = GeoChartColorAxis.create();
+		//geoChartColorAxis.setColors("green", "yellow", "red");
+		options.setColorAxis(geoChartColorAxis);
+		options.setDatalessRegionColor("gray");
 
-				// Draw the chart
-				geoChart.draw(dataTable, options);
+		// Draw the chart
+		geoChart.draw(dataTable, options);
+		
 	}
 
 }
--- IoTDashboard/war/index.jsp
+++ IoTDashboard/war/index.jsp
@@ -13,7 +13,9 @@
 <body>
 	<div id="root" style="display: none">
 		<jsp:include page="header.jsp" />
+
 		<div align="center" id="chart"></div>
+
 		<jsp:include page="footer.jsp" />
 	</div>
 </body>
