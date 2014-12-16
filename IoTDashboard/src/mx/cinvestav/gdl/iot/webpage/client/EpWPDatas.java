2de4c1a4b5cfa6536295516fbaa59100dd7a2495
Lea Vega <Lea Vega@LVR>
2014-12-15 19:21:11 -0600

    Charts running

--- IoTDashboard/src/mx/cinvestav/gdl/iot/webpage/client/EpWPDatas.java
+++ IoTDashboard/src/mx/cinvestav/gdl/iot/webpage/client/EpWPDatas.java
@@ -38,9 +38,10 @@ import com.googlecode.gwt.charts.client.options.VAxis;
 public class EpWPDatas implements EntryPoint
 {
 	private DialogBox dbWait = new DialogBox();
-	private LineChart lineChart;
-
+	
 	private VerticalPanel formPanel = new VerticalPanel();
+	
+	private VerticalPanel formChart = new VerticalPanel();
 
 	private FlexTable tableData = new FlexTable();
 	private ListBox lbController = new ListBox();
@@ -213,6 +214,8 @@ public class EpWPDatas implements EntryPoint
 			public void onClick(ClickEvent event)
 			{
 
+				formChart.clear();
+				
 				final String type = lbTypeSensor.getItemText(lbTypeSensor.getSelectedIndex());
 
 				index = 0;
@@ -239,24 +242,29 @@ public class EpWPDatas implements EntryPoint
 									@Override
 									public void onSuccess(final List<MeasureDTO> result)
 									{
+										
+										
 										ChartLoader chartLoader = new ChartLoader(ChartPackage.CORECHART);
 										chartLoader.loadApi(new Runnable()
 										{
 											@Override
 											public void run()
 											{
+												LineChart lineChart =new LineChart();
+												
 												DecoratorPanel chartPanel = new DecoratorPanel();
-												chartPanel.add(getLineChart());
+												chartPanel.add(lineChart);
+												formChart.add(chartPanel);
 
-												RootPanel.get("chart").add(chartPanel);
-
-												drawLineChart(result, name);
+												drawLineChart(lineChart,result, name);
 											}
 
 										});
 									}
 
 								});
+						
+						RootPanel.get("chart").add(formChart);
 
 					}
 
@@ -267,20 +275,12 @@ public class EpWPDatas implements EntryPoint
 
 	}
 
-	private LineChart getLineChart()
-	{
-		if (lineChart == null)
-		{
-			lineChart = new LineChart();
-		}
-		return lineChart;
-	}
 
-	private void drawLineChart(List<MeasureDTO> result, String name)
+	private void drawLineChart(LineChart lineChart, List<MeasureDTO> result, String name)
 	{
 
 
-		ArrayList<String> dates = new ArrayList<String>();
+	/*	ArrayList<String> dates = new ArrayList<String>();
 
 		for (int rows = 0; rows < result.size(); rows++)
 		{
@@ -316,7 +316,44 @@ public class EpWPDatas implements EntryPoint
 		options.setVAxis(VAxis.create("Value"));
 
 		// Draw the chart
+		lineChart.draw(dataTable, options);*/
+		
+		
+		
+		String[] countries = new String[] { "Austria", "Bulgaria", "Denmark", "Greece" };
+		int[] years = new int[] { 2003, 2004, 2005, 2006, 2007, 2008 };
+		Integer [][] values = new Integer[][] { { 1336060, 1538156, 1576579, 1600652, 1968113, 1901067 },
+				{ 400361, 366849, 440514, 434552, 393032, 517206 },
+				{ 1001582, 1119450, 993360, 1004163, 979198, 916965 },
+				{ 997974, 941795, 930593, 897127, 1080887, 1056036 } };
+
+		// Prepare the data
+		DataTable dataTable = DataTable.create();
+		dataTable.addColumn(ColumnType.STRING, "Year");
+		for (int i = 0; i < countries.length; i++) {
+			dataTable.addColumn(ColumnType.NUMBER, countries[i]);
+		}
+		dataTable.addRows(years.length);
+		for (int i = 0; i < years.length; i++) {
+			dataTable.setValue(i, 0, String.valueOf(years[i]));
+		}
+		for (int col = 0; col < values.length; col++) {
+			for (int row = 0; row < values[col].length; row++) {
+				dataTable.setValue(row, col + 1, values[col][row]);
+			}
+		}
+
+		// Set options
+		LineChartOptions options = LineChartOptions.create();
+		options.setBackgroundColor("#f0f0f0");
+		options.setFontName("Tahoma");
+		options.setTitle("Yearly Coffee Consumption by Country");
+		options.setHAxis(HAxis.create("Year"));
+		options.setVAxis(VAxis.create("Cups"));
+
+		// Draw the chart
 		lineChart.draw(dataTable, options);
+		
 	}
 
 	public void showDialogWait()
