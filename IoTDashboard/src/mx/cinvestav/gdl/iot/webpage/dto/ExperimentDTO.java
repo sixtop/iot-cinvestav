a8db16b12a61d4e75be1841069cf5f269e00a54b
Victor Palacios <sixtop@gmail.com>
2015-04-20 20:03:14 -0500

    Changed ExperimentDTO

--- IoTDashboard/src/mx/cinvestav/gdl/iot/webpage/dto/ExperimentDTO.java
+++ IoTDashboard/src/mx/cinvestav/gdl/iot/webpage/dto/ExperimentDTO.java
@@ -1,83 +1,91 @@
 package mx.cinvestav.gdl.iot.webpage.dto;
 
-import java.util.Date;
-import java.util.Map;
+import java.sql.Timestamp;
 
 public class ExperimentDTO implements IoTEntityDTO
 {
 	private static final long serialVersionUID = -1165471382042735357L;
 
-	private Integer idSmartThing;
+	private Integer id;
 	private String name;
 	private String description;
-	private Date dateStart;
-	private Date dateEnd;
+	private Timestamp start_date;
+	private Timestamp end_date;
+	private Integer idthing;
 	private String notes;
 	
 	public ExperimentDTO()
 	{
 		super();
 	}
-	
-	private Integer id;
-	public Integer getId() {
+
+	public Integer getId()
+	{
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Integer id)
+	{
 		this.id = id;
 	}
 
-	public Integer getIdSmartThing() {
-		return idSmartThing;
-	}
-
-	public void setIdSmartThing(Integer idSmartThing) {
-		this.idSmartThing = idSmartThing;
-	}
-
-	public String getName() {
+	public String getName()
+	{
 		return name;
 	}
 
-	public void setName(String name) {
+	public void setName(String name)
+	{
 		this.name = name;
 	}
 
-	public String getDescription() {
+	public String getDescription()
+	{
 		return description;
 	}
 
-	public void setDescription(String description) {
+	public void setDescription(String description)
+	{
 		this.description = description;
 	}
 
-	public Date getDateStart() {
-		return dateStart;
+	public Timestamp getStart_date()
+	{
+		return start_date;
 	}
 
-	public void setDateStart(Date dateStart) {
-		this.dateStart = dateStart;
+	public void setStart_date(Timestamp start_date)
+	{
+		this.start_date = start_date;
 	}
 
-	public Date getDateEnd() {
-		return dateEnd;
+	public Timestamp getEnd_date()
+	{
+		return end_date;
 	}
 
-	public void setDateEnd(Date dateEnd) {
-		this.dateEnd = dateEnd;
+	public void setEnd_date(Timestamp end_date)
+	{
+		this.end_date = end_date;
 	}
 
-	public String getNotes() {
+	public Integer getIdthing()
+	{
+		return idthing;
+	}
+
+	public void setIdthing(Integer idthing)
+	{
+		this.idthing = idthing;
+	}
+
+	public String getNotes()
+	{
 		return notes;
 	}
 
-	public void setNotes(String notes) {
+	public void setNotes(String notes)
+	{
 		this.notes = notes;
 	}
-
-
-
-
-
 }
